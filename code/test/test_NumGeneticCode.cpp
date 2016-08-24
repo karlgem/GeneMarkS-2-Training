2f2b682a60b90a1f609899e12ba8fdbd775c747f
Karl Gemayel <karl@gatech.edu>
2016-08-24 16:45:16 -0400

    Test genetic code for starts and stops

--- code/test/test_NumGeneticCode.cpp
+++ code/test/test_NumGeneticCode.cpp
@@ -10,6 +10,7 @@
 
 #include "catch.hpp"
 #include "GeneticCode.hpp"
+#include "AlphabetDNA.hpp"
 #include "NumGeneticCode.hpp"
 #include "CharNumConverter.hpp"
 
@@ -20,28 +21,40 @@
 using namespace std;
 using namespace gmsuite;
 
+
+void testGeneticCode(GeneticCode::gcode_t geneticCode) {
+    // create genetic code and numeric converter
+    AlphabetDNA alphabet;
+    GeneticCode gc(geneticCode);
+    CharNumConverter cnc(&alphabet);
+    
+    // create numeric code
+    NumGeneticCode gcNum (gc, cnc);
+    
+    for (GeneticCode::ttk_const_iterator iter = gc.begin(); iter != gc.end(); iter++) {
+        
+        // convert codon from string to seq_t
+        CharNumConverter::seq_t conv;
+        cnc.convert(*iter, conv);
+        
+        // make sure its status fits with original genetic code
+        REQUIRE(gcNum.isStart(conv) == gc.isStart(*iter));
+        REQUIRE(gcNum.isStop(conv) == gc.isStop(*iter));
+    }
+
+}
+
+
 TEST_CASE("Testing NumGeneticCode - ") {
     
-    
+    GeneticCode::gcode_t geneticCodes [2];
     
     SECTION("Numeric Genetic Code 11") {
-        
-        // create genetic code and numeric converter
-        AlphabetDNA alphabet;
-        GeneticCode gc(GeneticCode::ELEVEN);
-        CharNumConverter cnc(&alphabet);
-        
-        // create numeric code
-        NumGeneticCode gcNum (gc, cnc);
-        
-        SECTION("Starts") {
-            
-            // for each start in genetic code, make sure it is recognized by numeric genetic code
-            
-        }
-        
-        
-        
+        testGeneticCode(GeneticCode::ELEVEN);
+    }
+    
+    SECTION("Numeric Genetic Code 4") {
+        testGeneticCode(GeneticCode::FOUR);
     }
     
 }
