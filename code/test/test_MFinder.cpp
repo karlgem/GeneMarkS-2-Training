e2402b2706ea6f2b833b8b6fa43ae13ecdb8fea8
Karl Gemayel <karl@gatech.edu>
2016-09-15 09:23:52 -0400

    ADD: options to mfinder for testing

--- code/test/test_MFinder.cpp
+++ code/test/test_MFinder.cpp
@@ -21,10 +21,19 @@ TEST_CASE("Testing Motif Finder") {
     srand(1);
     
     OptionsMFinder options("mfinder");
+    options.align = "right";
+    options.width = 6;
+    options.motifOrder = 0;
+    options.bkgdOrder = 0;
+    options.pcounts = 1;
+    options.tries = 10;
+    options.maxIter = 60;
+    options.shiftEvery = 20;
+    options.maxEMIter = 10;
+    
     options.fname_in = "/Users/Karl/repos/GeneMarkS-2/code/tmp/ecoli-motifs.fa";
 //    options.fname_in = "/Users/Karl/repos/GeneMarkS-2/code/tmp/sample-motifs.fa";
 //    options.fname_in = "/Users/Karl/repos/GeneMarkS-2/code/tmp/sample-rand-motifs.fa";
-    options.pcounts = 0;
     
     ModuleMFinder mfinder (options);
     
