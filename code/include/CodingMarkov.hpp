551e6b14903cbc56367474b85b981be7d60393bf
Karl Gemayel <karl@gatech.edu>
2016-12-01 09:16:42 -0500

    ADD: CodingMarkov - markov model for coding regions
    Specifically, pseudocounts are not added to stop codons

---
+++ code/include/CodingMarkov.hpp
@@ -0,0 +1,45 @@
+//
+//  CodingMarkov.hpp
+//  GeneMark Suite
+//
+//  Created by Karl Gemayel on 12/1/16.
+//  Copyright © 2016 Karl Gemayel. All rights reserved.
+//
+
+#ifndef CodingMarkov_hpp
+#define CodingMarkov_hpp
+
+#include <stdio.h>
+#include "PeriodicMarkov.hpp"
+#include "NumGeneticCode.hpp"
+
+namespace gmsuite {
+    
+    /**
+     * @class CodingMarkov
+     * @brief A Markov model for coding regions.
+     */
+    class CodingMarkov : public PeriodicMarkov {
+        
+    public:
+        
+        /**
+         * Constructor:
+         *
+         * @param order the model's order
+         * @param period the model's period
+         * @param alphabet the alphabet used by the model
+         */
+        CodingMarkov(unsigned order, size_t period, const NumAlphabetDNA &alph, const NumGeneticCode &geneticCode);
+        
+    protected:
+        
+        const NumGeneticCode* geneticCode;      /**< Numeric version of the genetic code */
+        
+        //@override
+        void addPseudocounts(int pcount);
+        
+    };
+}
+
+#endif /* CodingMarkov_hpp */
