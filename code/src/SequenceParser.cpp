7cdd1c3a31b9b166c48abe27ea91a1afd04098b8
Karl Gemayel <karl@gatech.edu>
2017-11-18 15:36:51 -0500

    Fix reverse complement in sc extract

--- code/src/SequenceParser.cpp
+++ code/src/SequenceParser.cpp
@@ -168,7 +168,7 @@ NumSequence SequenceParser::extractStartContextSequence(const NumSequence& seque
         if (posRelToStart >= 0 && length-1 > right - posRelToStart) throw invalid_argument("Attempt to extract start context with left boundary less than 0");
         
         size_t fragLeft = right - posRelToStart - length + 1;
-        return sequence.subseq(fragLeft, length);
+        return sequence.subseq(fragLeft, length).reverseComplement(cnc);
         
     }
     else
