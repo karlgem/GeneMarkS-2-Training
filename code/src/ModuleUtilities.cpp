23fbb93cd40d05aa07f056c04d48e2b30ddd95f6
Karl Gemayel <karl@gatech.edu>
2018-10-24 22:06:25 -0400

    Change delimiter to ;

--- code/src/ModuleUtilities.cpp
+++ code/src/ModuleUtilities.cpp
@@ -1662,13 +1662,14 @@ void ModuleUtilities::runExtractORF() {
         
         // create fasta header
         stringstream ssmHeader;
-        string delimiter =  " ";
+        string interDelimiter =  ":";
+        string intraDelimiter =  ";";
         ssmHeader << ">" << currLabel->meta;
         if (!utilOpt.tag.empty())
-            ssmHeader << delimiter << "tag=" << utilOpt.tag;
-        ssmHeader << delimiter << "pos=" << posLeft+1 << "," << posRight+1 << "," << currLabel->strandToStr();
-        ssmHeader << delimiter << "cds=" << currLabel->left+1 << "," << currLabel->right+1 << "," << currLabel->strandToStr();
-        ssmHeader << delimiter << "type=" << (utilOpt.aa ? "prot" : "nucl");
+            ssmHeader << interDelimiter << "tag=" << utilOpt.tag;
+        ssmHeader << interDelimiter << "pos=" << posLeft+1 << intraDelimiter << posRight+1 << intraDelimiter << currLabel->strandToStr();
+        ssmHeader << interDelimiter << "cds=" << currLabel->left+1 << intraDelimiter << currLabel->right+1 << intraDelimiter << currLabel->strandToStr();
+        ssmHeader << interDelimiter << "type=" << (utilOpt.aa ? "prot" : "nucl");
         
         fastaHeader = ssmHeader.str();
         
